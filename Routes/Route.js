438259f41554aaad88ed7edbaf0700341df27531
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-02-27 21:18:32 +0530

    login server

--- Routes/Route.js
+++ Routes/Route.js
@@ -2,9 +2,13 @@ import express from "express";
 
 import { signup } from "../Controllers/Signup.js";
 
+import { login } from "../Controllers/Login.js";
 
 const Route = express.Router();
 
 Route.post("/signup",signup);
+
+Route.post("/login",login);
+
 
 export default Route;
