2dd06e30bb21b2541679c43a2c7d3df00e23104e
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-02-28 19:49:21 +0530

    tweet Message

--- Routes/Route.js
+++ Routes/Route.js
@@ -4,10 +4,14 @@ import { signup } from "../Controllers/Signup.js";
 
 import { login } from "../Controllers/Login.js";
 
+import {tweet} from "../Controllers/tweet.js"
+
 const Route = express.Router();
 
 Route.post("/signup", signup);
 
 Route.get("/login", login);
+
+Route.post("/tweet",tweet);
 
 export default Route;
