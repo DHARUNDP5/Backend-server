ed2c4f111ed98874549e1de65e86a7fb428c0afa
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-03-09 11:56:52 +0530

    delete tweet completed Message

--- Controllers/Deletetweet.js
+++ Controllers/Deletetweet.js
@@ -1,7 +1,13 @@
 import { uploadModel } from "../Schema/UploadSchema.js"
 
 export const deleteTweet = async (req, res) => {
+
     const id = req.params.id;
-    await uploadModel.findByIdAndRemove(id).exec();
-    return res.send("deleted");
+     uploadModel.deleteOne({
+      id:id
+     },(err,data)=>{
+if(data){
+return res.send("deleted");
+}
+     })
   }
