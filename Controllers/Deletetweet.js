9cde237d7385d78a397c0dcd597c4ebd48a715a0
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-03-09 11:58:27 +0530

    code formated

--- Controllers/Deletetweet.js
+++ Controllers/Deletetweet.js
@@ -2,12 +2,12 @@ import { uploadModel } from "../Schema/UploadSchema.js"
 
 export const deleteTweet = async (req, res) => {
 
-    const id = req.params.id;
-     uploadModel.deleteOne({
-      id:id
-     },(err,data)=>{
-if(data){
-return res.send("deleted");
+  const id = req.params.id;
+  uploadModel.deleteOne({
+    id: id
+  }, (err, data) => {
+    if (data) {
+      return res.send("deleted");
+    }
+  })
 }
-     })
-  }
